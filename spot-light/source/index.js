fe6368db2d2300ff05f3041694aa4939fe0588ea
Matrix <1337816495@qq.com>
2022-01-17 10:52:53 +0800

    Update index.js

--- spot-light/source/index.js
+++ spot-light/source/index.js
@@ -51,15 +51,6 @@ window.addEventListener("resize", _ => {
 
 });
 
-/* Render */
-renderer.setAnimationLoop(function loop() {
-
-    controls.update();
-
-    renderer.render(scene, camera);
-
-});
-
 /* ------------------------------------------------------------------------------------------------------ */
 /* Tetrahedron */
 const tetrahedron = new three.Mesh(
@@ -89,36 +80,105 @@ const ambient_light = new three.AmbientLight(0xffffff, 0.1);
 scene.add(ambient_light);
 
 /* Spot light */
-const light = new three.SpotLight(0xff00ff);
+const spot_light_1 = new SpotLight();
+const spot_light_2 = new SpotLight();
+const spot_light_3 = new SpotLight();
 
-light.position.set(5, 5, 5);
-light.target = tetrahedron;
-light.angle = 0.2;              // 照射范围。
-light.penumbra = 0.2;           // 半影衰减百分比。
-light.decay = 1;                // 随着光照距离的衰减量。
-light.distance = 15;            // 光照距离。
-light.castShadow = true;        // 启用阴影投射。
-light.shadow.mapSize.x = 1024;  // 阴影贴图的x。
-light.shadow.mapSize.y = 1024;  // 阴影贴图的y。
-light.shadow.camera.near = 0.1; // 阴影相机近端面界限。
-light.shadow.camera.far = 20;   // 阴影相机远端面界限。
+spot_light_1.position.set(0, 5, - 5);
+spot_light_1.target = tetrahedron;
+spot_light_1.color = new three.Color(0xff00ff);
 
-scene.add(light);
+spot_light_2.position.set(- 5 / 2 * Math.sqrt(3), 5, 5 / 2);
+spot_light_2.target = tetrahedron;
+spot_light_2.color = new three.Color(0x00ffff);
 
-const helper_1 = new three.SpotLightHelper(light);
+spot_light_3.position.set(5 / 2 * Math.sqrt(3), 5, 5 / 2);
+spot_light_3.target = tetrahedron;
+spot_light_3.color = new three.Color(0xffff00);
 
-scene.add(helper_1);
+scene.add(spot_light_1, spot_light_2, spot_light_3);
 
-gui.add(light.position, "x").min(- 10).max(10).step(0.1).name("X").onChange(updateLight);
-gui.add(light.position, "y").min(- 10).max(10).step(0.1).name("Y").onChange(updateLight);
-gui.add(light.position, "z").min(- 10).max(10).step(0.1).name("Z").onChange(updateLight);
-gui.add(light, "angle").min(0).max(Math.PI / 2).step(Math.PI / 100).name("Angle").onChange(updateLight);
-gui.add(light, "penumbra").min(0).max(1).step(0.1).name("Penumbra").onChange(updateLight);
-gui.add(light, "decay").min(0).max(10).step(0.1).name("Decay").onChange(updateLight);
-gui.add(light, "distance").min(0).max(40).step(0.1).name("Distance").onChange(updateLight);
+function SpotLight() {
+
+    const light = new three.SpotLight();
+
+    light.angle = 0.2;              // 照射范围。
+    // light.penumbra = 0.2;           // 半影衰减百分比。
+    // light.decay = 1;                // 随着光照距离的衰减量。
+    light.distance = 15;            // 光照距离。
+    light.castShadow = true;        // 启用阴影投射。
+    light.shadow.mapSize.x = 1024;  // 阴影贴图的x。
+    light.shadow.mapSize.y = 1024;  // 阴影贴图的y。
+    light.shadow.camera.near = 0.1; // 阴影相机近端面界限。
+    light.shadow.camera.far = 20;   // 阴影相机远端面界限。
+
+    return light;
+
+}
+
+/* Spot light helper */
+const light_helper_1 = new three.SpotLightHelper(spot_light_1);
+const light_helper_2 = new three.SpotLightHelper(spot_light_2);
+const light_helper_3 = new three.SpotLightHelper(spot_light_3);
+
+const camera_helper_1 = new three.CameraHelper(spot_light_1.shadow.camera);
+const camera_helper_2 = new three.CameraHelper(spot_light_2.shadow.camera);
+const camera_helper_3 = new three.CameraHelper(spot_light_3.shadow.camera);
+
+scene.add(light_helper_1, light_helper_2, light_helper_3);
+scene.add(camera_helper_1, camera_helper_2, camera_helper_3);
+
+// gui.add(light.position, "x").min(- 10).max(10).step(0.1).name("X").onChange(updateLight);
+// gui.add(light.position, "y").min(- 10).max(10).step(0.1).name("Y").onChange(updateLight);
+// gui.add(light.position, "z").min(- 10).max(10).step(0.1).name("Z").onChange(updateLight);
+// gui.add(light, "angle").min(0).max(Math.PI / 2).step(Math.PI / 100).name("Angle").onChange(updateLight);
+// gui.add(light, "penumbra").min(0).max(1).step(0.1).name("Penumbra").onChange(updateLight);
+// gui.add(light, "decay").min(0).max(10).step(0.1).name("Decay").onChange(updateLight);
+// gui.add(light, "distance").min(0).max(40).step(0.1).name("Distance").onChange(updateLight);
+
+// TODO 阴影BUG
 
 function updateLight() {
 
-    helper_1.update();
+    light_helper_1.update();
+    light_helper_2.update();
+    light_helper_3.update();
+
+    spot_light_1.shadow.camera.updateProjectionMatrix();
+    spot_light_2.shadow.camera.updateProjectionMatrix();
+    spot_light_3.shadow.camera.updateProjectionMatrix();
+
+    camera_helper_1.update();
+    camera_helper_2.update();
+    camera_helper_3.update();
 
 }
+
+/* ------------------------------------------------------------------------------------------------------ */
+/* Render */
+const clock = new three.Clock();
+
+let previous_time = 0;
+let elapsed_time = 0;
+let delta_time = 0;
+
+renderer.setAnimationLoop(function loop() {
+
+    /* Rotate tetrahedron */
+    elapsed_time = clock.getElapsedTime();
+    delta_time = elapsed_time - previous_time;
+    previous_time = elapsed_time;
+
+    tetrahedron.rotateY(delta_time * 1);
+    tetrahedron.rotateZ(delta_time * 1);
+
+    /* Update Light's helper */
+    updateLight();
+
+    /* Update Controls */
+    controls.update();
+
+    /* Render */
+    renderer.render(scene, camera);
+
+});
